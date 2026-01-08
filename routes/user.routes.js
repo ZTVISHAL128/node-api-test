90d9d65711adabc5f7de56178a12ec6e68220298
Vishal Shingar <vishal.shingar@zehntech.com>
2026-01-08 16:18:08 +0530

    improve the API

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,12 +3,16 @@ const router = express.Router();
 
 // GET users
 router.get("/", (req, res) => {
-  res.json([
+  res.json({
+    status: 200,
+    message: "User list fetched successfully",
+    data:
+    [
     { id: 1, name: "Vishal" },
     { id: 2, name: "Rahul" },
     { id: 3, name: "Yash" },
     { id: 4, name: "Rupesh" }
-  ]);
+  ]});
 });
 
 // POST user
